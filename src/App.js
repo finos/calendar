85415bc62a4a2c9473a89798a1892ce51785c676
Mimi <mimi.flynn@morganstanley.com>
2023-09-21 16:46:01 -0400

    display basic calendar with real events

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ import timeGridPlugin from '@fullcalendar/timegrid';
 
 import './App.css';
 
+function renderEventContent(info) {
+  return <b class="fc-event-title">{info.event.title}</b>;
+}
+
 function App() {
+  // Get the current date as a string in the format 'YYYY-MM-DD'
+  const currentDate = new Date().toISOString().slice(0, 10);
+
   return (
     <div className="App">
       <FullCalendar
@@ -17,6 +24,21 @@ function App() {
           iCalendarPlugin,
         ]}
         initialView="dayGridMonth"
+        events={{
+          url: 'basic.ics',
+          format: 'ics',
+        }}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek,timeGridDay',
+        }}
+        dayMaxEventRows={999}
+        initialDate={currentDate}
+        navLinks
+        editable
+        dayMaxEvents={false}
+        eventContent={renderEventContent}
       />
     </div>
   );
