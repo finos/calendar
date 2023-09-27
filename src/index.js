c22a4cbe010619cf280a535021f900b837faebcf
Juan Estrella <juan.estrella@finos.org>
2023-09-27 20:03:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -51,27 +51,9 @@ document.addEventListener('DOMContentLoaded', function() {
                     const startTime = info.event.start
                     const endTime = info.event.end
 
-                    const formattedStartTime = formatDate(startTime, {
-                        hour: 'numeric',
-                        year: 'numeric',
-                        month: 'long',
-                        day: 'numeric',
-                        minute: '2-digit',
-                        timeZoneName: 'long',
-                    });
-
-                    const formattedEndTime = formatDate(endTime, {
-                        hour: 'numeric',
-                        year: 'numeric',
-                        month: 'long',
-                        day: 'numeric',
-                        minute: '2-digit',
-                        timeZoneName: 'long',
-                    });
-
                     const modalContent = document.createElement('div');
                     modalContent.classList.add('modal-content');
-                    modalContent.innerHTML = `<b>${info.event.title}</b><br></br><strong>Start:</strong> ${formattedStartTime}<br><strong>End:</strong> ${formattedEndTime}<br><br>${info.event.extendedProps.description}<br>`;
+                    modalContent.innerHTML = `<b>${info.event.title}</b><br></br><strong>Start:</strong> ${startTime}<br><strong>End:</strong> ${endTime}<br><br>${info.event.extendedProps.description}<br>`;
                     modalContainer.appendChild(modalContent);
 
                     // Add a close button to the popup
