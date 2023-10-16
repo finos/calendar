f60f2698c4c6a01a796bd0979d19fe0ea18909e9
Juan Estrella <juan.estrella@finos.org>
2023-10-16 18:41:25 +0200

    remove GMT text

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import iCalendarPlugin from '@fullcalendar/icalendar';
 import interactionPlugin from '@fullcalendar/interaction';
 import parse from 'html-react-parser';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import momentTimezonePlugin from '@fullcalendar/moment-timezone'
+import momentTimezonePlugin from '@fullcalendar/moment-timezone';
 
 import useEscKey from './hooks/useEscKey';
 
@@ -14,87 +14,84 @@ import './App.css';
 const htmlRegex = /<\/*html-blob>/;
 
 function App() {
-  const calendarRef = createRef();
+	const calendarRef = createRef();
 
-  const [loading, setLoading] = useState(true);
-  const [showEventDetails, setShowEventDetails] = useState(false);
-  const [eventDetails, setEventDetails] = useState(false);
+	const [ loading, setLoading ] = useState(true);
+	const [ showEventDetails, setShowEventDetails ] = useState(false);
+	const [ eventDetails, setEventDetails ] = useState(false);
 
-  useEscKey(() => setShowEventDetails(false));
+	useEscKey(() => setShowEventDetails(false));
 
-  const handleEventClick = useCallback((clickInfo) => {
-    setEventDetails(clickInfo.event);
-    setShowEventDetails(true);
-  });
+	const handleEventClick = useCallback((clickInfo) => {
+		setEventDetails(clickInfo.event);
+		setShowEventDetails(true);
+	});
 
-  const renderEventDetails = () => {
-    const description = eventDetails.extendedProps.description.replace(
-      htmlRegex,
-      ''
-    );
+	const renderEventDetails = () => {
+		const description = eventDetails.extendedProps.description.replace(htmlRegex, '');
 
-    return (
-      <div className="finos-calendar-event-details">
-        <button
-          onClick={() => setShowEventDetails(false)}
-          className="fc-button finos-calendar-event-details-close"
-        >
-          X
-        </button>
-        <h2>{eventDetails.title}</h2>
-        <div>
-          <strong>Start:</strong> {eventDetails.start.toString()}
-        </div>
-        <div>
-          <strong>End:</strong> {eventDetails.end.toString()}
-        </div>
-        {parse(description)}
-      </div>
-    );
-  };
+		return (
+			<div className="finos-calendar-event-details">
+				<button
+					onClick={() => setShowEventDetails(false)}
+					className="fc-button finos-calendar-event-details-close"
+				>
+					X
+				</button>
+				<h2>{eventDetails.title}</h2>
+				<div>
+					<strong>Start:</strong> {eventDetails.start.toString().replace(/ GMT\+\d{4}\s/g, ' ')}
+				</div>
+				<div>
+					<strong>End:</strong> {eventDetails.end.toString().replace(/ GMT\+\d{4}\s/g, ' ')}
+				</div>
+				{parse(description)}
+			</div>
+		);
+	};
 
-  const renderFullCalendar = useMemo(
-    () => (
-      <FullCalendar
-        ref={calendarRef}
-        plugins={[
-          dayGridPlugin,
-          iCalendarPlugin,
-          interactionPlugin,
-          timeGridPlugin,
-          momentTimezonePlugin,
-        ]}
-        initialView="dayGridMonth"
-        events={{
-          url: 'basic.ics',
-          format: 'ics',
-        }}
-        headerToolbar={{
-          left: 'prev,next today',
-          center: 'title',
-          right: 'dayGridMonth,timeGridWeek,timeGridDay',
-        }}
-        dayMaxEventRows={999}
-        initialDate={new Date().toISOString().slice(0, 10)}
-        navLinks
-        editable
-        dayMaxEvents
-        timeZone='America/New_York'
-        eventClick={handleEventClick}
-        loading={(isLoading) => setLoading(isLoading)}
-      />
-    ),
-    []
-  );
+	const renderFullCalendar = useMemo(
+		() => (
+			<FullCalendar
+				ref={calendarRef}
+				plugins={[
+					dayGridPlugin,
+					iCalendarPlugin,
+					interactionPlugin,
+					timeGridPlugin,
+					momentTimezonePlugin
+				]}
+				initialView="dayGridMonth"
+				events={{
+					url    : 'basic.ics',
+					format : 'ics'
+				}}
+				headerToolbar={{
+					left   : 'prev,next today',
+					center : 'title',
+					right  : 'dayGridMonth,timeGridWeek,timeGridDay'
+				}}
+				dayMaxEventRows={999}
+				initialDate={new Date().toISOString().slice(0, 10)}
+				navLinks
+				editable
+				dayMaxEvents
+				timeZone="America/New_York"
+				eventClick={handleEventClick}
+				loading={(isLoading) => setLoading(isLoading)}
+			/>
+		),
+		[]
+	);
 
-  return (
-    <div className="App main">
-      <div className="finos-calendar">{renderFullCalendar}</div>
-      {showEventDetails && renderEventDetails()}
-      {loading && <div className="finos-calendar-overlay" />}
-      {loading && <div className="finos-calendar-loading">Loading...</div>}
-    </div>
-  );
+	return (
+		<div className="App main">
+			<div className="finos-calendar">{renderFullCalendar}</div>
+			{showEventDetails && renderEventDetails()}
+			{loading && <div className="finos-calendar-overlay" />}
+			{loading && <div className="finos-calendar-loading">Loading...</div>}
+		</div>
+	);
 }
 
 export default App;
