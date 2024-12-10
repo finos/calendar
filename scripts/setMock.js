066f1434ad3153be505fb208157eced0d2189150
Mimi <mimi.flynn@morganstanley.com>
2024-12-10 17:11:59 -0500

    create dist dir in mock script

--- scripts/setMock.js
+++ scripts/setMock.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 
 const sourceFile = './mock/events.json';
-const destinationFile = './dist/events.json';
+const destinationDirectory = './dist';
+const destinationFile = `${destinationDirectory}/events.json`;
+
+if (!fs.existsSync(destinationDirectory)) {
+  fs.mkdirSync(destinationDirectory);
+}
 
 fs.copyFile(sourceFile, destinationFile, (err) => {
   if (err) throw err;
